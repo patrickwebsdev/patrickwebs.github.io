9eea377c1a4c73ca07959832af12d631ae9ad407
BuildTools <unconfigured@null.spigotmc.org>
2021-07-26 03:25:31 -0300

    new file:   css/flickity.css 	deleted:    css/slick.css 	modified:   css/style.css 	modified:   css/style.min.css 	modified:   img/social_media_logo.png 	modified:   index.html 	new file:   js/flickity.js 	modified:   js/script.js 	modified:   js/script.min.js 	deleted:    js/slick.min.js

--- js/script.js
+++ js/script.js
@@ -4,36 +4,15 @@ $(document).ready(function() {
 		$(element).attr("src", img);
 	});
 	new PatrickWebs().init();
+	$(".scroll").click(function() {
+		var targetDiv = $(this).attr('href');
+		$('html, body').animate({
+			scrollTop: ($(targetDiv).offset().top-100)
+		}, 500);
+	});
+	$('.skills .item').flickity({
+		cellAlign: 'left',
+		contain: true,
+		pageDots: false,
+	});
 });
-$(".scroll").click(function() {
-	var targetDiv = $(this).attr('href');
-	$('html, body').animate({
-		scrollTop: ($(targetDiv).offset().top-100)
-	}, 500);
-});
-$('.skills .items').slick({
-	infinite: true,
-	slidesToShow: 5,
-	autoplay: true,
-	autoplaySpeed: 3000,
-	arrows: false,
-	draggable: true,
-	touchMove: true,
-	pauseOnHover: true,
-	responsive: [
-	  {
-		breakpoint: 769,
-		settings: {
-		  slidesToShow: 3,
-		  slidesToScroll: 1
-		}
-	  },
-	  {
-		breakpoint: 480,
-		settings: {
-		  slidesToShow: 2,
-		  slidesToScroll: 2
-		}
-	  }
-	]
-  });
